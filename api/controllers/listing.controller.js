6e77f4a6e21757480bb3f03fb3e38f0ca19c7a43
Usama <usamamukhtar.work@gmail.com>
2024-09-25 20:00:07 +0500

    Search API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -58,3 +58,51 @@ export const createListing = async (req, res, next) => {
         next(error);
         }
     }
+
+    export const getListings = async (req, res, next) =>{
+        try{
+            const limit = parseInt(req.query.limit) || 9;
+            const startIndex = parseInt(req.query.startIndex) || 0;
+
+            let offer = req.query.offer;
+            if(offer === undefined || offer === 'false'){
+                offer = {$in: [false, true]};
+            }
+
+            let furnished = req.query.furnished;
+            if(furnished === undefined || furnished === 'false'){
+                furnished = {$in: [false, true]}
+            } 
+
+            let parking = req.query.parking;
+            if(parking === undefined || parking === 'false'){
+                parking = {$in: [false, true]}
+            }
+
+            let type = req.query.type;
+            if(type === undefined || type === 'all'){
+                type = {$in: ['sale', 'rent']}
+            }
+
+            // Here $regex is functionality of a mongoDB that provide us a very smooth experience to search soething even if we write only a letter of the word rather to write the comlete word which we are searching,
+
+            // Here $options 'i' that means we do not need to worry about the text case that are in upper case or lower case it provide a good experience to search while we are searching in upper case or lower case.  
+
+            const searchTerm = req.query.searchTerm || '';
+            const sort = req.query.sort || 'createdAt';
+            const order = req.query.order || 'desc';
+
+            const listings = await Listing.find({
+            name: { $regex: searchTerm, $options: 'i' },
+            offer,
+            furnished,
+            parking,
+            type,
+            }).sort({[sort]: order}).limit(limit).skip(startIndex);
+
+            return res.status(200).json(listings);
+
+        }catch(error){
+            next(error);
+        }
+    }
